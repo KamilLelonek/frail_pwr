951e2806fe3153fd4dd3d60ddb4e27f96142432c
Kamil Lelonek <squixy.sln@gmail.com>
2014-05-02 13:41:11 +0200

    HTN finished

--- src/code/mkd/KamilLelonekHTNActorController.cpp
+++ src/code/mkd/KamilLelonekHTNActorController.cpp
@@ -5,81 +5,103 @@
 KamilLelonekHTNActorController::KamilLelonekHTNActorController( ActorAI* ai ) : IActorController(ai)
 {
     m_planner = new HTN::Planner();
-    m_planner->init(getAI()->getHtnMethodsPath(),getAI()->getHtnOperatorsPath(),getAI()->getHtnGoalsPath());
+    m_planner -> init(
+						getAI() -> getHtnMethodsPath(),
+						getAI() -> getHtnOperatorsPath(),
+						getAI() -> getHtnGoalsPath()
+					 );
+	
+	/*********************
+			ACTIONS
+	*********************/
+    m_actions["opPatrol"		]	= &KamilLelonekHTNActorController::actionPatrol;
+    m_actions["opAttackMelee"	]	= &KamilLelonekHTNActorController::actionAttackMelee;
+    m_actions["opRotateToEnemy" ]	= &KamilLelonekHTNActorController::actionRotateToEnemy;
+    m_actions["opAttackFireball"]	= &KamilLelonekHTNActorController::actionAttackFireball;
+    m_actions["opReduceDistance"]	= &KamilLelonekHTNActorController::actionReduceDistance;
+    m_actions["opRevealAttacker"]	= &KamilLelonekHTNActorController::actionRevealAttacker;
+    m_actions["opAngerMode"		]   = &KamilLelonekHTNActorController::actionAngerMode;
+    m_actions["opExploreSpot"	]	= &KamilLelonekHTNActorController::actionExploreSpot;
+    m_actions["opKeepDistance"	]	= &KamilLelonekHTNActorController::actionKeepDistance;
+    m_actions["opHeal"			]	= &KamilLelonekHTNActorController::actionHeal;
 
-    //actions////////////////////////////////////////////////////////////////////////
-    m_actions["opPatrol"] = &KamilLelonekHTNActorController::actionPatrol;
-    m_actions["opAttackMelee"] = &KamilLelonekHTNActorController::actionAttackMelee;
-    m_actions["opRotateToEnemy"] = &KamilLelonekHTNActorController::actionRotateToEnemy;
-    m_actions["opAttackFireball"] = &KamilLelonekHTNActorController::actionAttackFireball;
-    m_actions["opReduceDistance"] = &KamilLelonekHTNActorController::actionReduceDistance;
-    m_actions["opRevealAttacker"] = &KamilLelonekHTNActorController::actionRevealAttacker;
-    m_actions["opAngerMode"] = &KamilLelonekHTNActorController::actionAngerMode;
-    m_actions["opExploreSpot"] = &KamilLelonekHTNActorController::actionExploreSpot;
-    m_actions["opKeepDistance"] = &KamilLelonekHTNActorController::actionKeepDistance;
-    //////////////////////////////////////////////////////////////////////////
-    m_actions["animAngerMode"] = &KamilLelonekHTNActorController::animAngerMode;
+	/*********************
+		  ANIMATIONS
+	*********************/
+    m_actions["animAngerMode"]		= &KamilLelonekHTNActorController::animAngerMode;
     m_actions["animAttackFireball"] = &KamilLelonekHTNActorController::animAttackFireball;
-    m_actions["animAttackMelee"] = &KamilLelonekHTNActorController::animAttackMelee;
+    m_actions["animAttackMelee"]	= &KamilLelonekHTNActorController::animAttackMelee;
 
-    m_isAttacked = false;
-    m_enemyRunningAway = false;
-    m_prevEnemyDist = 1000.f;
+    m_isAttacked		= false;
+    m_enemyRunningAway	= false;
+    m_angerMode			= false;
+    m_prevEnemyDist		= 1000.f;
     m_prevEnemyDistTime = 0.f;
-    m_prevDistSum = 0.f;
-    m_angerMode = false;
-    m_enemyLastPos = mkVec3::ZERO;
-    m_target = NULL;
+    m_prevDistSum		= 0.f;
+    m_enemyLastPos		= mkVec3::ZERO;
+    m_target			= NULL;
 }
 
-KamilLelonekHTNActorController::~KamilLelonekHTNActorController(){
+KamilLelonekHTNActorController::~KamilLelonekHTNActorController() {
     delete m_planner;
 }
 
-//////////////////////////////////////////////////////////////////////////
+void KamilLelonekHTNActorController::onCreate() {
+    getAI() -> lookAt(mkVec3::ZERO);
 
-void KamilLelonekHTNActorController::onCreate(){
-    getAI()->lookAt(mkVec3::ZERO);
-    //ai_specific////////////////////////////////////////////////////////////////////////
-    m_planner->setStateFloat("rngMelee",getAI()->getMeleeRange());
-    m_planner->setStateFloat("rngFbMax",getAI()->getShootingRange());
-    m_planner->setStateFloat("dmgConeSize",getAI()->getMeleeConeSize());
-    m_planner->setStateFloat("HealthAMLimit",(getAI()->getMaxHealth())/2);
-    //conditions////////////////////////////////////////////////////////////////////////
-    m_planner->setStateBool("IsEnemyVisible",false);
-    m_planner->setStateBool("IsEnemyDead",false);
-    m_planner->setStateBool("IsEnemyAttack",false);
-    m_planner->setStateBool("IsEnemyRunningAway",false);
-    m_planner->setStateBool("IsActorAM",false);
-    m_planner->setStateBool("IsEnemySeen",false);
-    m_planner->setStateBool("IsSpotReached",false);
-    m_planner->setStateFloat("EnemyDistance",FLT_MAX);
-    m_planner->setStateFloat("ActorHealth",0.f);
-    m_planner->setStateFloat("EnemyDgrDiff",0.f);
-
-    Ogre::LogManager::getSingleton().logMessage("HTN controller created!");
+	m_planner -> setStateFloat("rngMelee",		 getAI() -> getMeleeRange());
+    m_planner -> setStateFloat("rngFbMax",		 getAI() -> getShootingRange());
+    m_planner -> setStateFloat("dmgConeSize",	 getAI() -> getMeleeConeSize());
+    m_planner -> setStateFloat("HealthAMLimit", (getAI() -> getMaxHealth()) / 2);
+    m_planner -> setStateBool("IsEnemyVisible",		false);
+    m_planner -> setStateBool("IsEnemyDead",		false);
+    m_planner -> setStateBool("IsEnemyAttack",		false);
+    m_planner -> setStateBool("IsEnemyRunningAway",	false);
+    m_planner -> setStateBool("IsActorAM",			false);
+    m_planner -> setStateBool("IsEnemySeen",		false);
+    m_planner -> setStateBool("IsSpotReached",		false);
+    m_planner -> setStateBool("IsActorHealthy",		true);
+    m_planner -> setStateFloat("EnemyDistance", FLT_MAX);
+    m_planner -> setStateFloat("ActorHealth",  0.f);
+    m_planner -> setStateFloat("EnemyDgrDiff", 0.f);
 }
 
-void KamilLelonekHTNActorController::onTakeDamage(const SDamageInfo& dmg_info){
+void KamilLelonekHTNActorController::onTakeDamage(const SDamageInfo& dmg_info) {
     m_isAttacked = true;
     m_attackDir = mkVec3::ZERO-dmg_info.dir;
 }
 
-void KamilLelonekHTNActorController::onUpdate(float dt){
+bool KamilLelonekHTNActorController::isActorHealthy()
+{
+	return ((getAI() -> getHealth()) > ((getAI() -> getMaxHealth()) / 2));
+}
+
+void KamilLelonekHTNActorController::onUpdate(float dt)
+{	
+	if(getAI() -> isInPowerLake() && !KamilLelonekHTNActorController::isActorHealthy()) 
+	{
+		getAI() -> setSpeed(0.f);
+	}
+
     updateWorldState(dt);
 
-    std::vector<HTN::pTask> plan = m_planner->getPlan(getAI()->getHtnMethodsPath(),getAI()->getHtnOperatorsPath(),getAI()->getHtnGoalsPath());
+    std::vector<HTN::pTask> plan = 
+								m_planner -> getPlan(
+														getAI() -> getHtnMethodsPath(),
+														getAI() -> getHtnOperatorsPath(),
+														getAI() -> getHtnGoalsPath()
+													);
     HTN::pOperator newTask;
-    HTN::PlanResult result = m_planner->resolvePlan(plan, dt, newTask);
+    HTN::PlanResult result = m_planner -> resolvePlan(plan, dt, newTask);
 
     switch (result)
     {
     case HTN::PLAN_EMPTYPLAN:
-        getAI()->setSpeed(0.f);
+        getAI() -> setSpeed(0.f);
         break;
     case HTN::PLAN_INTERRUPTED:
-        getAI()->stopSmoothChangeDir();
-        getAI()->stopAnimation();
+        getAI() -> stopSmoothChangeDir();
+        getAI() -> stopAnimation();
     case HTN::PLAN_NEW:
         executeTask(newTask);
         break;
@@ -88,10 +110,12 @@ void KamilLelonekHTNActorController::onUpdate(float dt){
     }
 }
 
-void KamilLelonekHTNActorController::onDebugDraw(){
-    getAI()->drawSensesInfo();
+void KamilLelonekHTNActorController::onDebugDraw() {
+    getAI() -> drawSensesInfo();
     if(m_enemyLastPos != mkVec3::ZERO)
+	{
         DebugDrawer::getSingleton().drawCircle(m_enemyLastPos, 0.2f, 30, Ogre::ColourValue::Black, true);
+	}
 }
 
 void KamilLelonekHTNActorController::onDie()
@@ -99,31 +123,26 @@ void KamilLelonekHTNActorController::onDie()
     m_angerMode = false;
 }
 
-//////////////////////////////////////////////////////////////////////////
+void KamilLelonekHTNActorController::updateWorldState(float dt) {
+	m_planner -> setStateBool("IsEnemyAttack", m_isAttacked);
+    m_planner -> setStateFloat("ActorHealth", getAI() -> getHealth());
+    m_planner -> setStateBool("IsActorHealthy",	KamilLelonekHTNActorController::isActorHealthy());
+    m_planner -> setStateBool("IsActorAM", m_angerMode);
 
-void KamilLelonekHTNActorController::updateWorldState(float dt){
-    if(m_isAttacked)
-        m_planner->setStateBool("IsEnemyAttack",true);
-    else
-        m_planner->setStateBool("IsEnemyAttack",false);
-    
-    m_planner->setStateFloat("ActorHealth",getAI()->getHealth());
-    m_planner->setStateBool("IsActorAM",m_angerMode);
-
-    m_target = getAI()->findClosestEnemyInSight();
-    m_planner->setStateBool("IsEnemyVisible",m_target ? true : false);
+    m_target = getAI() -> findClosestEnemyInSight();
+    m_planner -> setStateBool("IsEnemyVisible",m_target ? true : false);
     if(m_target){
-        float angleDiff = (float)(getAI()->getCharToEnemyAngle(m_target->getSimPos()).valueDegrees());
-        m_planner->setStateFloat("EnemyDgrDiff",angleDiff);
+        float angleDiff = (float)(getAI() -> getCharToEnemyAngle(m_target -> getSimPos()).valueDegrees());
+        m_planner -> setStateFloat("EnemyDgrDiff",angleDiff);
         ActorAI *targetAI = dynamic_cast<ActorAI*>(m_target);
         Player *targetPlayer = dynamic_cast<Player*>(m_target);
         if(targetAI)
-            m_planner->setStateBool("IsEnemyDead",targetAI->getHealth() > 0.f ? false : true);
+            m_planner -> setStateBool("IsEnemyDead",targetAI -> getHealth() > 0.f ? false : true);
         else if(targetPlayer)
-            m_planner->setStateBool("IsEnemyDead",targetPlayer->getHealth() > 0.f ? false : true);
+            m_planner -> setStateBool("IsEnemyDead",targetPlayer -> getHealth() > 0.f ? false : true);
 
-        float enemyDistance = (float)(m_target->getSimPos() - getAI()->getSimPos()).length();
-        m_planner->setStateFloat("EnemyDistance",enemyDistance);
+        float enemyDistance = (float)(m_target -> getSimPos() - getAI() -> getSimPos()).length();
+        m_planner -> setStateFloat("EnemyDistance",enemyDistance);
 
         m_prevDistSum += enemyDistance - m_prevEnemyDist;
         m_prevEnemyDist = enemyDistance;
@@ -138,44 +157,54 @@ void KamilLelonekHTNActorController::updateWorldState(float dt){
             m_prevEnemyDistTime = 0.5f;
         }
 
-        m_planner->setStateBool("IsEnemyRunningAway",m_enemyRunningAway);
+        m_planner -> setStateBool("IsEnemyRunningAway",m_enemyRunningAway);
 
-        m_enemyLastPos = m_target->getSimPos();
-        m_planner->setStateBool("IsEnemySeen",true);
+        m_enemyLastPos = m_target -> getSimPos();
+        m_planner -> setStateBool("IsEnemySeen",true);
     } else {
-        m_planner->setStateBool("IsEnemyVisible",false);
-        m_planner->setStateBool("IsEnemyDead",false);
-        m_planner->setStateBool("IsEnemyRunningAway",false);
-        m_planner->setStateFloat("EnemyDistance",1000.f);
-        m_planner->setStateFloat("EnemyDistanceDiff",0.f);
-        m_planner->setStateFloat("EnemyDgrDiff",0.f);
+        m_planner -> setStateBool("IsEnemyVisible",false);
+        m_planner -> setStateBool("IsEnemyDead",false);
+        m_planner -> setStateBool("IsEnemyRunningAway",false);
+        m_planner -> setStateFloat("EnemyDistance",1000.f);
+        m_planner -> setStateFloat("EnemyDistanceDiff",0.f);
+        m_planner -> setStateFloat("EnemyDgrDiff",0.f);
     }
 
-    if((float)(m_enemyLastPos - getAI()->getSimPos()).length() < 3.f){
-        m_planner->setStateBool("IsSpotReached",true);
-        m_planner->setStateBool("IsEnemySeen",false);
+    if((float)(m_enemyLastPos - getAI() -> getSimPos()).length() < 3.f){
+        m_planner -> setStateBool("IsSpotReached",true);
+        m_planner -> setStateBool("IsEnemySeen",false);
     } else {
-        m_planner->setStateBool("IsSpotReached",false);
+        m_planner -> setStateBool("IsSpotReached",false);
     }
 }
 
-void KamilLelonekHTNActorController::executeTask(HTN::pOperator nextTask){
-    ctrlrAction action = m_actions[nextTask->getName()];
-    (this->*action)(nextTask->getDuration());
+void KamilLelonekHTNActorController::executeTask(HTN::pOperator nextTask) {
+	
+	if(KamilLelonekHTNActorController::isActorHealthy())
+	{
+		ctrlrAction action = m_actions[nextTask -> getName()];
+		(this ->* action)(nextTask -> getDuration());
+	}
+	else
+	{
+		KamilLelonekHTNActorController::actionHeal(nextTask -> getDuration());
+	}
 }
 
-//----------actions----------
+/*********************
+		ACTIONS
+*********************/
 bool KamilLelonekHTNActorController::actionPatrol(float duration){
     mkVec3 new_direction = getRandomHorizontalDir();
-    RayCastResult ray_result = getAI()->raycast(new_direction, 1.0f, 5.f);
+    RayCastResult ray_result = getAI() -> raycast(new_direction, 1.0f, 5.f);
     while(ray_result.hit && ray_result.collision_type == RayCastResult::Environment){
         new_direction = getRandomHorizontalDir();
-        ray_result = getAI()->raycast(new_direction, 1.0f, 5.f);
+        ray_result = getAI() -> raycast(new_direction, 1.0f, 5.f);
     }
 
     size_t steps = 40;
-    getAI()->startSmoothChangeDir(new_direction, steps, duration/2);
-    getAI()->setSpeed(0.5f);
+    getAI() -> startSmoothChangeDir(new_direction, steps, duration/2);
+    getAI() -> setSpeed(0.5f);
 
     return true;
 }
@@ -185,10 +214,10 @@ bool KamilLelonekHTNActorController::actionRotateToEnemy(float duration){
         return false;
 
     size_t steps = 40;
-    mkVec3 new_direction = m_target->getSimPos() - getAI()->getSimPos();
+    mkVec3 new_direction = m_target -> getSimPos() - getAI() -> getSimPos();
     new_direction.normalise();
-    getAI()->startSmoothChangeDir(new_direction, steps, duration);
-    getAI()->setSpeed(0.f);
+    getAI() -> startSmoothChangeDir(new_direction, steps, duration);
+    getAI() -> setSpeed(0.f);
 
     return true;
 }
@@ -197,8 +226,8 @@ bool KamilLelonekHTNActorController::actionAttackMelee(float duration){
     if(!m_target)
         return false;
 
-    getAI()->hitMelee();
-    getAI()->setSpeed(0.f);
+    getAI() -> hitMelee();
+    getAI() -> setSpeed(0.f);
 
     return true;
 }
@@ -208,31 +237,31 @@ bool KamilLelonekHTNActorController::actionAttackFireball(float duration){
         return false;
 
     
-    getAI()->hitFireball(m_target->getSimPos());
-    getAI()->setSpeed(0.f);
+    getAI() -> hitFireball(m_target -> getSimPos());
+    getAI() -> setSpeed(0.f);
     return true;
 }
 
 bool KamilLelonekHTNActorController::actionReduceDistance(float duration){
     if(!m_target)
         return false;
-    RayCastResult ray_result = getAI()->raycast(m_target->getSimPos(), 0.1f, 1.f);
+    RayCastResult ray_result = getAI() -> raycast(m_target -> getSimPos(), 0.1f, 1.f);
     if (ray_result.hit && ray_result.collision_type == RayCastResult::Environment)
-        getAI()->jump();
+        getAI() -> jump();
 
-    mkVec3 destDir = (m_target->getSimPos()-getAI()->getSimPos()).normalisedCopy();
+    mkVec3 destDir = (m_target -> getSimPos()-getAI() -> getSimPos()).normalisedCopy();
     size_t steps = 40;
-    getAI()->startSmoothChangeDir(destDir, steps, duration);
-    getAI()->setSpeed(1.f);
+    getAI() -> startSmoothChangeDir(destDir, steps, duration);
+    getAI() -> setSpeed(1.f);
 
     return true;
 }
 
 bool KamilLelonekHTNActorController::actionRevealAttacker(float duration){
-    getAI()->setSpeed(0.3f);
+    getAI() -> setSpeed(0.3f);
     size_t steps = 40;
     m_isAttacked = false;
-    getAI()->startSmoothChangeDir(m_attackDir, steps, (duration+duration+duration)/4);
+    getAI() -> startSmoothChangeDir(m_attackDir, steps, (duration+duration+duration)/4);
 
     return true;
 }
@@ -241,7 +270,7 @@ bool KamilLelonekHTNActorController::actionAngerMode(float duration){
     if(!m_target)
         return false;
 
-    getAI()->hitAngerMode();
+    getAI() -> hitAngerMode();
     m_angerMode = true;
     return true;
 }
@@ -252,10 +281,10 @@ bool KamilLelonekHTNActorController::actionExploreSpot(float duration)
         return false;
 
     size_t steps = 40;
-    mkVec3 new_direction = m_enemyLastPos - getAI()->getSimPos();
+    mkVec3 new_direction = m_enemyLastPos - getAI() -> getSimPos();
     new_direction.normalise();
-    getAI()->startSmoothChangeDir(new_direction, steps, duration);
-    getAI()->setSpeed(1.f);
+    getAI() -> startSmoothChangeDir(new_direction, steps, duration);
+    getAI() -> setSpeed(1.f);
     return true;
 }
 
@@ -265,49 +294,65 @@ bool KamilLelonekHTNActorController::actionKeepDistance(float duration)
         return false;
 
     size_t steps = 40;
-    mkVec3 new_direction = getAI()->getSimPos() - m_target->getSimPos();
+    mkVec3 new_direction = getAI() -> getSimPos() - m_target -> getSimPos();
     new_direction.normalise();
 
-    RayCastResult ray_result = getAI()->raycast(new_direction, 1.0f, 10.f);
+    RayCastResult ray_result = getAI() -> raycast(new_direction, 1.0f, 10.f);
     if(ray_result.hit && ray_result.collision_type == RayCastResult::Environment){
         return false;
     } else {
-        getAI()->startSmoothChangeDir(new_direction, steps, duration/3);
-        getAI()->setSpeed(1.0f);
+        getAI() -> startSmoothChangeDir(new_direction, steps, duration/3);
+        getAI() -> setSpeed(1.0f);
         return true;
     }
     return false;
 }
 
+bool KamilLelonekHTNActorController::actionHeal(float duration)
+{
+	ActorAI* actor = getAI();
+	if(actor -> isMedkitAvailable())
+	{
+		actor -> lookAt(getAI() -> getMedkitPosition());
+	}
+	else
+	{
+		actor -> lookAt(actor -> getPowerLakePosition());
+	}
+    actor -> setSpeed(10.f);
+
+	return true;
+}
+
+/*********************
+	  ANIMATIONS
+*********************/
 bool KamilLelonekHTNActorController::animAngerMode( float duration )
 {
-    if(!m_target)
-        return false;
+    if(!m_target) return false;
 
-    getAI()->runAnimation("HighJump",duration);
-    getAI()->setSpeed(0.f);
+    getAI() -> runAnimation("HighJump", duration);
+    getAI() -> setSpeed(0.f);
 
     return true;
 }
 
 bool KamilLelonekHTNActorController::animAttackMelee( float duration )
 {
-    if(!m_target)
-        return false;
+    if(!m_target) return false;
 
-    getAI()->runAnimation("Attack3",duration);
-    getAI()->setSpeed(0.f);
+    getAI() -> runAnimation("Attack3", duration);
+    getAI() -> setSpeed(0.f);
 
     return true;
 }
 
 bool KamilLelonekHTNActorController::animAttackFireball( float duration )
 {
-    if(!m_target)
-        return false;
+    if(!m_target) return false;
 
-    getAI()->runAnimation("Attack1",duration);
-    getAI()->setSpeed(0.f);
+    getAI() -> runAnimation("Attack1", duration);
+    getAI() -> setSpeed(0.f);
 
     return true;
 }
--- src/code/mkd/KamilLelonekHTNActorController.h
+++ src/code/mkd/KamilLelonekHTNActorController.h
@@ -17,34 +17,46 @@ public:
 	virtual void onUpdate(float dt);
 	virtual void onDebugDraw();
     virtual void onDie();
+
 private:
     std::map<std::string, ctrlrAction> m_actions;
     HTN::Planner *m_planner;
     Character *m_target;
+
     mkVec3 m_enemyLastPos;
-    bool m_isAttacked;
     mkVec3 m_attackDir;
+
+    bool m_isAttacked;
     bool m_enemyRunningAway;
+    bool m_angerMode;
+
     float m_prevEnemyDist;
     float m_prevDistSum;
     float m_prevEnemyDistTime;
-    bool m_angerMode;
 
     void updateWorldState(float dt);
     void executeTask(HTN::pOperator op);
 
-	//----------actions----------
-	bool actionPatrol(float duration);
-    bool actionRotateToEnemy(float duration);
-	bool actionAttackMelee(float duration);
-	bool actionAttackFireball(float duration);
-	bool actionReduceDistance(float duration);
-	bool actionRevealAttacker(float duration);
-    bool actionAngerMode(float duration);
-    bool actionExploreSpot(float duration);
-    bool actionKeepDistance(float duration);
-    //////////////////////////////////////////////////////////////////////////
-    bool animAngerMode(float duration);
-    bool animAttackMelee(float duration);
-    bool animAttackFireball(float duration);
+	bool isActorHealthy();
+	
+	/*********************
+			ACTIONS
+	*********************/
+	bool actionPatrol			(float duration);
+    bool actionRotateToEnemy	(float duration);
+	bool actionAttackMelee		(float duration);
+	bool actionAttackFireball	(float duration);
+	bool actionReduceDistance	(float duration);
+	bool actionRevealAttacker	(float duration);
+    bool actionAngerMode		(float duration);
+    bool actionExploreSpot		(float duration);
+    bool actionKeepDistance		(float duration);
+    bool actionHeal				(float duration);
+
+	/*********************
+		  ANIMATIONS
+	*********************/
+    bool animAngerMode			(float duration);
+    bool animAttackMelee		(float duration);
+    bool animAttackFireball		(float duration);
 };
